7256733b147c21daf85d02b9d0fe1657a8c17a3c
Luiz <lvj253@gmail.com>
2021-07-18 16:57:10 -0300

    adicionando dotenv

--- src/database.js
+++ src/database.js
@@ -5,7 +5,7 @@ const { Pool } = pg;
 const connection = new Pool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  port: parseInt(process.env.DB_PASSWORD),
+  port: parseInt(process.env.DB_PORT),
   host: process.env.DB_HOST,
   database: process.env.DB_DATABASE
 });
--- src/server.js
+++ src/server.js
@@ -2,5 +2,5 @@ import './setup.js';
 import app from "./app.js";
 
 app.listen(4000, () => {
-  console.log(`Server is listening on port 4000.`);
+  console.log(`Server is listening on port ${4000}.`);
 });
