3dafb69d3665d6ecbd7b5498f6b7ef4b4ef1828f
Luiz <lvj253@gmail.com>
2021-07-18 15:45:29 -0300

    rota get completa

--- src/app.js
+++ src/app.js
@@ -13,9 +13,19 @@ app.post("/items", async (req, res) => {
             return res.sendStatus(400);
         }
         const text = req.body.text;
-        const newText = connection.query(`INSERT INTO items(text) VALUES($1)`, [text]);
+        const newText = await connection.query(`INSERT INTO items(text) VALUES($1)`, [text]);
         return res.sendStatus(201);
 
+    } catch{
+        return res.sendStatus(500);
+    }
+});
+
+app.get("/items", async (req, res) => {
+    try{
+        const texts = await connection.query(`SELECT * from items`);
+        res.status(200);
+        return res.send(texts.rows);
     } catch{
         return res.sendStatus(500);
     }
