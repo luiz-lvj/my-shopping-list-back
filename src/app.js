2cccc6d96a165d3ae8d787433ba06d138eb5ece1
Luiz <lvj253@gmail.com>
2021-07-18 15:42:31 -0300

    rota post completa

--- src/app.js
+++ src/app.js
@@ -7,4 +7,18 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+app.post("/items", async (req, res) => {
+    try{
+        if(!req.body.text || req.body.text === ""){
+            return res.sendStatus(400);
+        }
+        const text = req.body.text;
+        const newText = connection.query(`INSERT INTO items(text) VALUES($1)`, [text]);
+        return res.sendStatus(201);
+
+    } catch{
+        return res.sendStatus(500);
+    }
+});
+
 export default app;
